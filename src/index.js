cad0c4d904f45d26cb6f37179f35a0eed248236d
mrcaidev <mrcaidev@gmail.com>
2023-02-03 01:24:59 +0800

    feat: support custom dark selector

--- src/index.js
+++ src/index.js
@@ -65,14 +65,7 @@ const transformRadixColors = () => {
 
 module.exports = plugin(
   ({ addComponents, config, theme }) => {
-    const prefix = config("prefix");
-
-    const darkSelector =
-      config("darkMode") === "media"
-        ? "@media (prefers-color-scheme: dark)"
-        : prefix
-        ? `[class="${prefix}dark"] &`
-        : '[class="dark"] &';
+    const darkSelector = getDarkSelector(config);
 
     for (const [colorName, color] of Object.entries(theme("colors"))) {
       if (!(colorName in naturalColorPairs)) {
@@ -206,3 +199,40 @@ module.exports = plugin(
     },
   }
 );
+
+function getDarkSelector(config) {
+  const darkMode = config("darkMode");
+  const prefix = config("prefix");
+
+  if (Array.isArray(darkMode)) {
+    if (darkMode.length < 2) {
+      throw new Error(
+        "To customize the dark mode selector, `darkMode` should contain two items. Documentation: https://tailwindcss.com/docs/dark-mode#customizing-the-class-name"
+      );
+    }
+
+    if (darkMode[0] !== "class") {
+      throw new Error(
+        'To customize the dark mode selector, `darkMode` should have "class" as its first item. Documentation: https://tailwindcss.com/docs/dark-mode#customizing-the-class-name'
+      );
+    }
+
+    return darkMode[1] + " &";
+  }
+
+  if (darkMode === "media") {
+    return "@media (prefers-color-scheme: dark)";
+  }
+
+  if (darkMode !== "class") {
+    throw new Error(
+      "Invalid `darkMode`. Documentation: https://tailwindcss.com/docs/dark-mode"
+    );
+  }
+
+  if (prefix) {
+    return `[class="${prefix}dark"] &`;
+  }
+
+  return '[class="dark"] &';
+}
