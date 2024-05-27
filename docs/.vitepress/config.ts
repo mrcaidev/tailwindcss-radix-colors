699143cd17896c36fb08e94f95ab59e8106cca67
Yuwang Cai <mrcaidev@gmail.com>
2024-05-27 20:29:56 +0800

    docs: landing page

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,88 @@
+import { defineConfig } from "vitepress";
+
+export default defineConfig({
+  title: "Tailwind Radix Colors",
+  description:
+    "Bring Radix's Color System to Tailwind, and enjoy the best of both Tailwind CSS and Radix UI",
+  head: [
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/svg+xml",
+        href: "/tailwindcss.svg",
+      },
+    ],
+  ],
+  cleanUrls: true,
+  lastUpdated: true,
+  themeConfig: {
+    logo: "/tailwindcss.svg",
+    nav: [
+      {
+        text: "Tailwind CSS",
+        link: "https://tailwindcss.com",
+      },
+      {
+        text: "Radix Colors",
+        link: "https://www.radix-ui.com/colors",
+      },
+    ],
+    sidebar: [
+      {
+        text: "Introduction",
+        items: [
+          {
+            text: "Getting Started",
+            link: "/getting-started",
+          },
+          {
+            text: "Why another palette?",
+            link: "/why-another-palette",
+          },
+          {
+            text: "Why this plugin?",
+            link: "/why-this-plugin",
+          },
+        ],
+      },
+      {
+        text: "Guide",
+        items: [
+          {
+            text: "Utility First",
+            link: "/utility-first",
+          },
+          {
+            text: "Semantics First",
+            link: "/semantics-first",
+          },
+        ],
+      },
+      {
+        text: "Reference",
+        items: [
+          {
+            text: "Options",
+            link: "/options",
+          },
+        ],
+      },
+    ],
+
+    socialLinks: [
+      {
+        icon: "github",
+        link: "https://github.com/mrcaidev/tailwindcss-radix-colors",
+      },
+    ],
+    footer: {
+      message: "Released under the MIT license.",
+      copyright: "© 2024-present Yuwang Cai",
+    },
+    editLink: {
+      pattern:
+        "https://github.com/mrcaidev/tailwindcss-radix-colors/edit/master/docs/:path",
+    },
+  },
+});
