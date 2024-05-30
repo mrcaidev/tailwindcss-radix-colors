d052e244999ee149bb28525a203f5f1914329168
Yuwang Cai <mrcaidev@gmail.com>
2024-05-30 12:57:12 +0800

    chore(config): consistent eslint cache file name with prettier

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 export default {
   "*.{js,ts}": [
-    "eslint --fix --cache --cache-location ./node_modules/.cache/eslint/.eslintcache",
+    "eslint --fix --cache --cache-location ./node_modules/.cache/eslint/.eslint-cache",
     "prettier --write --cache",
   ],
   "*.{json,md}": "prettier --write --cache",
