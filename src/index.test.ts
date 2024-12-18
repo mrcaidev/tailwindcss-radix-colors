a7dd2e2d4a95ee2f769518863f689aeac1509fec
Yuwang Cai <mrcaidev@gmail.com>
2024-12-18 23:36:16 +0800

    build: bump tailwindcss to 3.4.17

--- src/index.test.ts
+++ src/index.test.ts
@@ -37,16 +37,16 @@ test("Given no option, both utility and semantic classes are generated", async (
   const expected = `
     .bg-slate-1 {
       --tw-bg-opacity: 1;
-      background-color: rgb(252 252 253 / var(--tw-bg-opacity));
+      background-color: rgb(252 252 253 / var(--tw-bg-opacity, 1));
     }
     .bg-slate-app {
       --tw-bg-opacity: 1;
-      background-color: rgb(252 252 253 / var(--tw-bg-opacity));
+      background-color: rgb(252 252 253 / var(--tw-bg-opacity, 1));
     }
     @media (prefers-color-scheme: dark) {
       .bg-slate-app {
         --tw-bg-opacity: 1;
-        background-color: rgb(17 17 19 / var(--tw-bg-opacity));
+        background-color: rgb(17 17 19 / var(--tw-bg-opacity, 1));
       }
     }
   `;
@@ -62,7 +62,7 @@ test("Given option `disableSemantics`, semantic classes are not generated", asyn
   const expected = `
     .bg-slate-1 {
       --tw-bg-opacity: 1;
-      background-color: rgb(252 252 253 / var(--tw-bg-opacity))
+      background-color: rgb(252 252 253 / var(--tw-bg-opacity, 1))
     }
   `;
 
@@ -78,7 +78,7 @@ test("Given option `include`, only specified colors are generated", async () => 
   const expected = `
     .bg-slate-1 {
       --tw-bg-opacity: 1;
-      background-color: rgb(252 252 253 / var(--tw-bg-opacity));
+      background-color: rgb(252 252 253 / var(--tw-bg-opacity, 1));
     }
   `;
 
@@ -105,7 +105,7 @@ test("Given option `priority=radix-only`, Tailwind colors are preserved", async 
   const expected = `
     .bg-zinc-100 {
       --tw-bg-opacity: 1;
-      background-color: rgb(244 244 245 / var(--tw-bg-opacity));
+      background-color: rgb(244 244 245 / var(--tw-bg-opacity, 1));
     }
   `;
 
@@ -121,7 +121,7 @@ test("Given option `priority=tailwind-first`, Tailwind colors take precedence", 
   const expected = `
     .bg-red-100 {
       --tw-bg-opacity: 1;
-      background-color: rgb(254 226 226 / var(--tw-bg-opacity));
+      background-color: rgb(254 226 226 / var(--tw-bg-opacity, 1));
     }
   `;
 
@@ -137,11 +137,11 @@ test("Given option `aliases`, conflicted color names are both preserved", async 
   const expected = `
     .bg-red-700 {
       --tw-bg-opacity: 1;
-      background-color: rgb(185 28 28 / var(--tw-bg-opacity));
+      background-color: rgb(185 28 28 / var(--tw-bg-opacity, 1));
     }
     .bg-sun-9 {
       --tw-bg-opacity: 1;
-      background-color: rgb(229 72 77 / var(--tw-bg-opacity));
+      background-color: rgb(229 72 77 / var(--tw-bg-opacity, 1));
     }
   `;
 
@@ -162,11 +162,11 @@ test("Utility classes are generated for custom colors", async () => {
   const expected = `
     .bg-custom1 {
       --tw-bg-opacity: 1;
-      background-color: rgb(18 52 86 / var(--tw-bg-opacity));
+      background-color: rgb(18 52 86 / var(--tw-bg-opacity, 1));
     }
     .bg-custom2-1 {
       --tw-bg-opacity: 1;
-      background-color: rgb(101 67 33 / var(--tw-bg-opacity));
+      background-color: rgb(101 67 33 / var(--tw-bg-opacity, 1));
     }
   `;
 
@@ -194,48 +194,48 @@ test("CSS classes are in correct shape", async () => {
   const expected = `
     .bg-slate-app {
       --tw-bg-opacity: 1;
-      background-color: rgb(252 252 253 / var(--tw-bg-opacity));
+      background-color: rgb(252 252 253 / var(--tw-bg-opacity, 1));
     }
     @media (prefers-color-scheme: dark) {
       .bg-slate-app {
         --tw-bg-opacity: 1;
-        background-color: rgb(17 17 19 / var(--tw-bg-opacity));
+        background-color: rgb(17 17 19 / var(--tw-bg-opacity, 1));
       }
     }
     .bg-slate-subtle {
       --tw-bg-opacity: 1;
-      background-color: rgb(249 249 251 / var(--tw-bg-opacity));
+      background-color: rgb(249 249 251 / var(--tw-bg-opacity, 1));
     }
     @media (prefers-color-scheme: dark) {
       .bg-slate-subtle {
         --tw-bg-opacity: 1;
-        background-color: rgb(24 25 27 / var(--tw-bg-opacity));
+        background-color: rgb(24 25 27 / var(--tw-bg-opacity, 1));
       }
     }
     .bg-slate-ui {
       --tw-bg-opacity: 1;
-      background-color: rgb(240 240 243 / var(--tw-bg-opacity));
+      background-color: rgb(240 240 243 / var(--tw-bg-opacity, 1));
     }
     .bg-slate-ui:hover {
       --tw-bg-opacity: 1;
-      background-color: rgb(232 232 236 / var(--tw-bg-opacity));
+      background-color: rgb(232 232 236 / var(--tw-bg-opacity, 1));
     }
     .bg-slate-ui:active {
       --tw-bg-opacity: 1;
-      background-color: rgb(224 225 230 / var(--tw-bg-opacity));
+      background-color: rgb(224 225 230 / var(--tw-bg-opacity, 1));
     }
     @media (prefers-color-scheme: dark) {
       .bg-slate-ui {
         --tw-bg-opacity: 1;
-        background-color: rgb(33 34 37 / var(--tw-bg-opacity));
+        background-color: rgb(33 34 37 / var(--tw-bg-opacity, 1));
       }
       .bg-slate-ui:hover {
         --tw-bg-opacity: 1;
-        background-color: rgb(39 42 45 / var(--tw-bg-opacity));
+        background-color: rgb(39 42 45 / var(--tw-bg-opacity, 1));
       }
       .bg-slate-ui:active {
         --tw-bg-opacity: 1;
-        background-color: rgb(46 49 53 / var(--tw-bg-opacity));
+        background-color: rgb(46 49 53 / var(--tw-bg-opacity, 1));
       }
     }
     .bg-slate-ghost {
@@ -243,11 +243,11 @@ test("CSS classes are in correct shape", async () => {
     }
     .bg-slate-ghost:hover {
       --tw-bg-opacity: 1;
-      background-color: rgb(232 232 236 / var(--tw-bg-opacity));
+      background-color: rgb(232 232 236 / var(--tw-bg-opacity, 1));
     }
     .bg-slate-ghost:active {
       --tw-bg-opacity: 1;
-      background-color: rgb(224 225 230 / var(--tw-bg-opacity));
+      background-color: rgb(224 225 230 / var(--tw-bg-opacity, 1));
     }
     @media (prefers-color-scheme: dark) {
       .bg-slate-ghost {
@@ -255,113 +255,113 @@ test("CSS classes are in correct shape", async () => {
       }
       .bg-slate-ghost:hover {
         --tw-bg-opacity: 1;
-        background-color: rgb(39 42 45 / var(--tw-bg-opacity));
+        background-color: rgb(39 42 45 / var(--tw-bg-opacity, 1));
       }
       .bg-slate-ghost:active {
         --tw-bg-opacity: 1;
-        background-color: rgb(46 49 53 / var(--tw-bg-opacity));
+        background-color: rgb(46 49 53 / var(--tw-bg-opacity, 1));
       }
     }
     .bg-slate-action {
       --tw-bg-opacity: 1;
-      background-color: rgb(232 232 236 / var(--tw-bg-opacity));
+      background-color: rgb(232 232 236 / var(--tw-bg-opacity, 1));
     }
     .bg-slate-action:hover {
       --tw-bg-opacity: 1;
-      background-color: rgb(224 225 230 / var(--tw-bg-opacity));
+      background-color: rgb(224 225 230 / var(--tw-bg-opacity, 1));
     }
     .bg-slate-action:active {
       --tw-bg-opacity: 1;
-      background-color: rgb(217 217 224 / var(--tw-bg-opacity));
+      background-color: rgb(217 217 224 / var(--tw-bg-opacity, 1));
     }
     @media (prefers-color-scheme: dark) {
       .bg-slate-action {
         --tw-bg-opacity: 1;
-        background-color: rgb(39 42 45 / var(--tw-bg-opacity));
+        background-color: rgb(39 42 45 / var(--tw-bg-opacity, 1));
       }
       .bg-slate-action:hover {
         --tw-bg-opacity: 1;
-        background-color: rgb(46 49 53 / var(--tw-bg-opacity));
+        background-color: rgb(46 49 53 / var(--tw-bg-opacity, 1));
       }
       .bg-slate-action:active {
         --tw-bg-opacity: 1;
-        background-color: rgb(54 58 63 / var(--tw-bg-opacity));
+        background-color: rgb(54 58 63 / var(--tw-bg-opacity, 1));
       }
     }
     .border-slate-dim {
       --tw-border-opacity: 1;
-      border-color: rgb(217 217 224 / var(--tw-border-opacity));
+      border-color: rgb(217 217 224 / var(--tw-border-opacity, 1));
     }
     @media (prefers-color-scheme: dark) {
       .border-slate-dim {
         --tw-border-opacity: 1;
-        border-color: rgb(54 58 63 / var(--tw-border-opacity));
+        border-color: rgb(54 58 63 / var(--tw-border-opacity, 1));
       }
     }
     .border-slate-normal {
       --tw-border-opacity: 1;
-      border-color: rgb(205 206 214 / var(--tw-border-opacity));
+      border-color: rgb(205 206 214 / var(--tw-border-opacity, 1));
     }
     .border-slate-normal:hover {
       --tw-border-opacity: 1;
-      border-color: rgb(185 187 198 / var(--tw-border-opacity));
+      border-color: rgb(185 187 198 / var(--tw-border-opacity, 1));
     }
     @media (prefers-color-scheme: dark) {
       .border-slate-normal {
         --tw-border-opacity: 1;
-        border-color: rgb(67 72 78 / var(--tw-border-opacity));
+        border-color: rgb(67 72 78 / var(--tw-border-opacity, 1));
       }
       .border-slate-normal:hover {
         --tw-border-opacity: 1;
-        border-color: rgb(90 97 105 / var(--tw-border-opacity));
+        border-color: rgb(90 97 105 / var(--tw-border-opacity, 1));
       }
     }
     .divide-slate-dim > :not([hidden]) ~ :not([hidden]) {
       --tw-divide-opacity: 1;
-      border-color: rgb(217 217 224 / var(--tw-divide-opacity));
+      border-color: rgb(217 217 224 / var(--tw-divide-opacity, 1));
     }
     @media (prefers-color-scheme: dark) {
       .divide-slate-dim > :not([hidden]) ~ :not([hidden]) {
         --tw-divide-opacity: 1;
-        border-color: rgb(54 58 63 / var(--tw-divide-opacity));
+        border-color: rgb(54 58 63 / var(--tw-divide-opacity, 1));
       }
     }
     .divide-slate-normal > :not([hidden]) ~ :not([hidden]) {
       --tw-divide-opacity: 1;
-      border-color: rgb(205 206 214 / var(--tw-divide-opacity));
+      border-color: rgb(205 206 214 / var(--tw-divide-opacity, 1));
     }
     .divide-slate-normal:hover > :not([hidden]) ~ :not([hidden]) {
       --tw-divide-opacity: 1;
-      border-color: rgb(185 187 198 / var(--tw-divide-opacity));
+      border-color: rgb(185 187 198 / var(--tw-divide-opacity, 1));
     }
     @media (prefers-color-scheme: dark) {
       .divide-slate-normal > :not([hidden]) ~ :not([hidden]) {
         --tw-divide-opacity: 1;
-        border-color: rgb(67 72 78 / var(--tw-divide-opacity));
+        border-color: rgb(67 72 78 / var(--tw-divide-opacity, 1));
       }
       .divide-slate-normal:hover > :not([hidden]) ~ :not([hidden]) {
         --tw-divide-opacity: 1;
-        border-color: rgb(90 97 105 / var(--tw-divide-opacity));
+        border-color: rgb(90 97 105 / var(--tw-divide-opacity, 1));
       }
     }
     .text-slate-dim {
       --tw-text-opacity: 1;
-      color: rgb(96 100 108 / var(--tw-text-opacity));
+      color: rgb(96 100 108 / var(--tw-text-opacity, 1));
     }
     @media (prefers-color-scheme: dark) {
       .text-slate-dim {
         --tw-text-opacity: 1;
-        color: rgb(176 180 186 / var(--tw-text-opacity));
+        color: rgb(176 180 186 / var(--tw-text-opacity, 1));
       }
     }
     .text-slate-normal {
       --tw-text-opacity: 1;
-      color: rgb(28 32 36 / var(--tw-text-opacity));
+      color: rgb(28 32 36 / var(--tw-text-opacity, 1));
     }
     @media (prefers-color-scheme: dark) {
       .text-slate-normal {
         --tw-text-opacity: 1;
-        color: rgb(237 238 240 / var(--tw-text-opacity));
+        color: rgb(237 238 240 / var(--tw-text-opacity, 1));
       }
     }
   `;
